835c7d77e0cc80d51636933b91855bd469db349d
Md Muktadir Nayem <mmuktadir629@gmail.com>
2025-01-11 23:31:49 +0600

    learning python

---
+++ fundamentals/fundamental.py
@@ -0,0 +1,23 @@
+#Hello world in python
+
+print("Hello world!")
+
+#python comments 
+
+#I am single line comments
+
+'''
+i
+ am
+   multiline
+ comments
+'''
+
+#python block of code simple:
+
+if 10>5:
+    print("this is true")
+    print("i am tab identation")
+
+print("i have no identation")
+
