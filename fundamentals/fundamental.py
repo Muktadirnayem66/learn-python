ca774138ff84bdedf9aa5e4e847d2d9da81206cd
Md Muktadir Nayem <mmuktadir629@gmail.com>
2025-01-12 22:38:35 +0600

    learning python

--- fundamentals/fundamental.py
+++ fundamentals/fundamental.py
@@ -1,6 +1,6 @@
 #Hello world in python
 
-print("Hello world!")
+#print("Hello world!")
 
 #python comments 
 
@@ -15,9 +15,27 @@ i
 
 #python block of code simple:
 
-if 10>5:
-    print("this is true")
-    print("i am tab identation")
+# if 10>5:
+#     print("this is true")
+#     print("i am tab identation")
 
-print("i have no identation")
+# print("i have no identation")
+
+# single variable
+# s = "Bob"
+# print(s)
+
+#multiple variable 
+# s = "Alice"
+# age = 25
+# city = "new yourk"
+# print(s, age, city)
+
+# amount = 150.75
+# print("Amount : ${:.2f}".format(amount))
+
+
+#input
+name = input("Enter your name:")
+print("Hello", name, "Welcome!")
 
